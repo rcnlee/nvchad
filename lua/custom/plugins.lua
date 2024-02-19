aac765cdc6270726ee1433fdde45d334c48382ae
Ritchie Lee <ritchie.lee@getcruise.com>
2024-02-19 01:30:36 +0000

    add syntax highlighting parsers

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -1,9 +1,36 @@
 local plugins = {
-  { "alexghergh/nvim-tmux-navigation", 
+  { "alexghergh/nvim-tmux-navigation",
     lazy = false,
     config = function()
       require'nvim-tmux-navigation'.setup{}
     end
+  },
+  { "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        -- defaults
+        "vim",
+        "lua",
+
+        -- programs 
+        "bash",
+        "python",
+        --"julia",
+
+        -- web
+        "csv",
+        "html",
+        "json",
+        "markdown",
+        "proto",
+        "sql",
+        "yaml",
+
+        -- low level 
+        "c",
+        "cpp",
+      }
+    }
   },
 }
 
